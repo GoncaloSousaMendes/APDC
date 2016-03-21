b130ca962c0801c083613622e24a8935d216e61b
unknown <gs.mendes@campus.fct.unl.pt>
2016-03-21 11:42:26 +0000

    21MAR16 - manha

--- quateriongenDist.py
+++ quateriongenDist.py
@@ -10,11 +10,8 @@ def random_quaternions(count=100):
     return a matrix with count random quaternions
     each quaternion is a line
     """
-	#matrix de count por 3, cheios de numeros aleatorios
     rands = np.random.rand(count,3)
-	# todos as posições zero de cada linha
     root_1 = np.sqrt(rands[:,0])
-	# equivalente a math.sqrt(1-u1), usando a primeira posição de cada linha
     minus_root_1 = np.sqrt(1-rands[:,0])
     two_pi_2 = np.pi*2*rands[:,1]
     two_pi_3 = np.pi*2*rands[:,2]
@@ -48,8 +45,7 @@ def multiply_quaternions(quats1,quats2):
     res[:,0] = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
     res[:,1] = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
     res[:,2] = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
-    #res[:,3] = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
-    res[:,3] = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
+    res[:,3] = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2    
     return res
     
 def conjugate(quats):
@@ -73,16 +69,10 @@ def rotate_points(points,quaternions):
     result is a matrix with (quaternions, points, 3) dimensions
     """
     
-    res = np.zeros((quaternions.shape[0],points.shape[0],4))  
-    #print res	
-    # cada matrix sera igual ao points
-    res[:,:,1:] = points   
-    #print res	
-	# vai buscar os conjugates...
+    res = np.zeros((quaternions.shape[0],points.shape[0],4))    
+    res[:,:,1:] = points    
     conjugates = conjugate(quaternions)    
-    #print conjugates
     
-	# ficamos com o ponto 'rodado' 
     for ix in range(len(points)):
         res[:,ix,:] = multiply_quaternions(quaternions,res[:,ix,:])
         res[:,ix,:] = multiply_quaternions(res[:,ix,:],conjugates)
@@ -107,33 +97,20 @@ def spread_quaternions(points,num=100,quats_per_step=100):
     """
     return a spread of num quaternions
     """
-	#cria uma matrix de zeros num por 4
     quats = np.zeros((num,4))
-    #print quats
-	# cria 'num' matrizes, cada uma com points.shape[0] linhas e 3 colunas
     rot_points = np.zeros((num,points.shape[0],3))
-    #print rot_points
-	#a primeira posição de quats fica =
     quats[0,:] = [0,0,0,1]
-    #print quats
-	# primeiro conjunto de matrizes fica igual aos points
     rot_points[0,:,:] = points
-    #print rot_points
     for ix in range(1,num):
-		# criar quats_per_step quaterniões aleatorios 
-        rand_quats = random_quaternions(quats_per_step) 
-		# fazer a rotação dos pontos usando os quaterniões aleatorios
-        positioned = rotate_points(points,rand_quats)  
-		# ver as distancias
+        rand_quats = random_quaternions(quats_per_step)        
+        positioned = rotate_points(points,rand_quats)      
         dists = point_dists(rot_points[:ix],positioned)
-		# ir buscar o maximo das distancias mais proximas
-        new_rot = np.argmax(dists)
-		
+        new_rot = np.argmax(dists)        
         quats[ix,:]=rand_quats[new_rot,:]
         rot_points[ix,:,:] = positioned[new_rot,:,:]
         
     return quats,rot_points
-        
+    
 def evaluate(rot_points):
     """
     return array with min distances
@@ -167,7 +144,7 @@ quaternions_per_set = 100
 #print points
 start_time = time.time()
 
-quats,rots = spread_quaternions(points,100,quaternions_per_set)
+quats,rots = spread_quaternions(points,1000,quaternions_per_set)
 print "elapsed:",time.time()-start_time
 
 start_time = time.time()
@@ -175,3 +152,6 @@ mins = evaluate(rots)
 print "elapsed eval:",time.time()-start_time
 
 draw_kde(mins,'distribution_'+str(quaternions_per_set)+'.png')
+
+
+
